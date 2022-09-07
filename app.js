5e2ac3e5b88b456b978f8c38b6c8415f32d1531b
BimalshaHasanthi <bimalshahasanthi@gmail.com>
2022-09-07 18:17:57 +0530

    Added Item

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require('express')
 
 const customer = require('./routes/customer')
 const item = require('./routes/item')
-const user = require ('./routes/user')
-const order = require ('./routes/Order')
-const orderDetail = require ('./routes/OrderDetails')
+// const user = require ('./routes/user')
+// const order = require ('./routes/Order')
+// const orderDetail = require ('./routes/OrderDetails')
 
 const app = express()
 const port = 4000
@@ -13,9 +13,9 @@ const port = 4000
 app.use(express.json())
 app.use('/customer', customer)
 app.use('/items',  item)
-app.use('/users',user)
-app.use('/orders',order)
-app.use('/orderDetail',orderDetail)
+// app.use('/users',user)
+// app.use('/orders',order)
+// app.use('/orderDetail',orderDetail)
 
 
 
